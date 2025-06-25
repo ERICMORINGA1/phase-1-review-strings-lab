e2c4060bd812e9ab055296f5facedb0437e4396d
ERICMORINGA1 <eyaleric@gmail.com>
2025-06-25 22:47:29 +0300

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = "Eric Omollo";
+
+const welcomeMessage = "Welcome to Flatbook, " + "Eric Omollo" + "!";
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + "Eric Omollo".toUpperCase() + "!";
+
+const shortGreeting = "Welcome, " + "Eric Omollo"[0] + "!";
